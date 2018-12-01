f14a16e9a4d5ee70628057f8b5fecfbadfc529ed
Pavel Pozdeev <pasharik@mail.ru>
2018-12-01 14:41:26 +0000

    build jar

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-jar cvf target/word.jar -C target/classes/ ru/pasharik/WordCountJob.class
+#jar cvf target/word.jar -C target/classes/ ru/pasharik/WordCountJob.class
+jar cvf target/word.jar -C target/classes/ .

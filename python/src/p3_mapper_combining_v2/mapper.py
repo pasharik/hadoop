77aa46650a05ec08f5fa119ed797d338eb5cfd63
Pavel Pozdeev <pasharik@mail.ru>
2019-01-24 17:00:30 +0000

    Small refactoring

--- python/src/p3_mapper_combining_v2/mapper.py
+++ python/src/p3_mapper_combining_v2/mapper.py
@@ -9,9 +9,9 @@ for line in sys.stdin:
     for token in line.strip().split(" "):
         if token:
             if a.get(token):
-                a[str(token)] += 1
+                a[token] += 1
             else:
-                a[str(token)] = 1
+                a[token] = 1
 
 for k in a.items():
     print(k[0] + '\t' + str(k[1]))

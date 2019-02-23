f1b776775c8ee1815351075576977e8335faa414
Pavel Pozdeev <pasharik@mail.ru>
2019-02-23 14:13:44 +0000

    Reducer union of two sets, i.e. just print all (unique) keys

---
+++ python/src/p14_reducer_union/reducer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+
+# Union of two sets, i.e. just print all (unique) keys
+# cd python/src/p14_reducer_union/
+# python3 reducer.py < test_data_for_reducer.txt
+
+lastKey = None
+
+for line in sys.stdin:
+    key = line.strip().split("\t")[0]
+    if lastKey and lastKey != key:
+        print(lastKey)
+    lastKey = key
+if lastKey:
+    print(lastKey)
+

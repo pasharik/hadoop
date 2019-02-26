0636c0213969de5e1174c7ac8e703c8dbea9b8e4
Pavel Pozdeev <pasharik@mail.ru>
2019-02-26 19:04:27 +0000

    Reducer symmetric difference between two sets - only unique elements

---
+++ python/src/p17_reducer_symmecric_difference/reducer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys
+
+# Subtract elements B from A
+# cd python/src/p17_reducer_symmecric_difference/
+# python3 reducer.py < test_data_for_reducer.txt
+
+oldLastKey = None
+(lastKey, lastValue) = (None, None)
+
+for line in sys.stdin:
+    item = line.strip().split("\t")
+    key = item[0]
+    value = item[1]
+    if lastKey and lastKey != key:
+        if ((not oldLastKey) or (oldLastKey != lastKey)):
+            print(lastKey)
+        oldLastKey = lastKey
+        (lastKey, lastValue) = (key, value)
+    else:
+        oldLastKey = lastKey
+        (lastKey, lastValue) = (key, value)
+
+if lastKey:
+    if ((not oldLastKey) or (oldLastKey != lastKey)):
+        print(lastKey)
